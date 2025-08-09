a946f6e934a5fc341fb615bf98f7d27b18c5c5c9
Tim <mortalitygames@gmail.com>
2025-08-09 20:05:40 +0200

    Added an 'info button' to allow for varions information to be displayed.

--- SF2CoreLibrary/src/com/sfc/sf2/core/gui/ExampleMainEditor.java
+++ SF2CoreLibrary/src/com/sfc/sf2/core/gui/ExampleMainEditor.java
@@ -59,6 +59,8 @@ public final class ExampleMainEditor extends AbstractMainEditor {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         fileButton1 = new com.sfc.sf2.core.gui.controls.FileButton();
+        infoButton1 = new com.sfc.sf2.core.gui.controls.InfoButton();
+        jLabel1 = new javax.swing.JLabel();
         console1 = new com.sfc.sf2.core.gui.controls.Console();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -122,15 +124,27 @@ public final class ExampleMainEditor extends AbstractMainEditor {
         fileButton1.setFilePath(".\\newFolder\\file.txt");
         fileButton1.setLabelText("Test file button :");
 
+        infoButton1.setBackground(new java.awt.Color(0, 0, 0));
+        infoButton1.setMessageText("<html>This is a test message. See how the info buttons can teach you.<br>You can set the <b>\"messageText\"</b> field to make a message popup by default, or you can listen to the <b>ActionPerformed</b> event.<br><br><i>If the text has any newlines then html tags don't seem to work.</i></html>");
+        infoButton1.setText("");
+
+        jLabel1.setText("Test help button:");
+
         javax.swing.GroupLayout jPanel15Layout = new javax.swing.GroupLayout(jPanel15);
         jPanel15.setLayout(jPanel15Layout);
         jPanel15Layout.setHorizontalGroup(
             jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel15Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(fileButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                .addGroup(jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(fileButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel15Layout.createSequentialGroup()
+                        .addGap(29, 29, 29)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(infoButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(table1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -146,6 +160,10 @@ public final class ExampleMainEditor extends AbstractMainEditor {
                     .addComponent(table1, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
                     .addGroup(jPanel15Layout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(44, 44, 44)
+                        .addGroup(jPanel15Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                            .addComponent(infoButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(fileButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(90, Short.MAX_VALUE))
@@ -207,6 +225,8 @@ public final class ExampleMainEditor extends AbstractMainEditor {
     private com.sfc.sf2.core.gui.controls.ColorPicker colorPicker1;
     private com.sfc.sf2.core.gui.controls.Console console1;
     private com.sfc.sf2.core.gui.controls.FileButton fileButton1;
+    private com.sfc.sf2.core.gui.controls.InfoButton infoButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel13;
     private javax.swing.JPanel jPanel15;
---
+++ SF2CoreLibrary/src/com/sfc/sf2/core/gui/controls/InfoButton.java
@@ -0,0 +1,57 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
+package com.sfc.sf2.core.gui.controls;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.BeanProperty;
+import javax.swing.JOptionPane;
+import javax.swing.event.EventListenerList;
+
+/**
+ *
+ * @author TiMMy
+ */
+public class InfoButton extends javax.swing.JButton {
+    
+    private final com.formdev.flatlaf.icons.FlatHelpButtonIcon flatHelpButtonIcon1;
+    private String messageText;
+    
+    public InfoButton() {
+        flatHelpButtonIcon1 = new com.formdev.flatlaf.icons.FlatHelpButtonIcon();
+        setMaximumSize(new java.awt.Dimension(20, 20));
+        setMinimumSize(new java.awt.Dimension(20, 20));
+        setPreferredSize(new java.awt.Dimension(20, 20));
+        setText(null);
+        setIcon(flatHelpButtonIcon1);
+        setMargin(new java.awt.Insets(1, 1, 1, 1));
+        setBorderPainted(false);
+        setContentAreaFilled(false); 
+        setFocusPainted(false); 
+        setOpaque(false);
+        this.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                buttonPressed(e);
+            }
+        });
+    }
+    
+    public String getMessageText() {
+        return messageText;
+    }
+    
+    @BeanProperty(preferred = true, visualUpdate = true, description = "The title to show on the Color Chooser popup.")
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+    
+    private void buttonPressed(ActionEvent evt) {
+        if (messageText != null) {
+            JOptionPane.showMessageDialog(this, messageText);
+        }
+    }
+}
